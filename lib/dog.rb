2b73d4a6b528bf0481f08f8f9bf48647767054a7
Scott Gale <galejscott@gmail.com>
2019-05-14 20:24:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,17 @@
-class Dog 
-  def initialize(name)
-    @name = name
-  end
- 
+class Dog
   def name
     @name
   end
-  
-#  def initialize(breed)
-#    @breed = breed 
-#  end
-  
+
+  def name=(name)
+    @name = name
+  end
+
   def breed
     @breed
   end
+
+  def breed=(breed)
+    @breed = breed
+  end
 end
-
-
-
-fido = Dog.new("Fido")
-fido.name
-
-snoopy = Dog.new("Beagle")
-snoopy.breed
